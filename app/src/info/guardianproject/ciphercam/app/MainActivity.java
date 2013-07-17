6bca49e27cf0c1a3d81991cb7e3fe31b2010b672
Josh Steiner <josh@vitriolix.com>
2013-07-17 14:42:54 -0700

    add test camera app that writes to standard storage first

--- app/src/info/guardianproject/ciphercam/app/MainActivity.java
+++ app/src/info/guardianproject/ciphercam/app/MainActivity.java
@@ -1,17 +1,48 @@
 package info.guardianproject.ciphercam.app;
 
+import java.io.IOException;
+
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.util.Log;
 import android.view.Menu;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener, SurfaceHolder.Callback {
+	private final String TAG = "MainActivity";
+	MediaRecorder recorder;
+    SurfaceHolder holder;
+    boolean recording = false;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+        recorder = new MediaRecorder();
+        initRecorder();
+        setContentView(R.layout.activity_main);
 
+        SurfaceView cameraView = (SurfaceView) findViewById(R.id.CameraView);
+        holder = cameraView.getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        cameraView.setClickable(true);
+        cameraView.setOnClickListener(this);
+    }
+    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -19,4 +50,63 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+    private void initRecorder() {
+        recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
+        recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+
+        CamcorderProfile cpHigh = CamcorderProfile
+                .get(CamcorderProfile.QUALITY_HIGH);
+        recorder.setProfile(cpHigh);
+        String filePath = Environment.getExternalStorageDirectory().getPath() + "/videocapture_example.mp4";
+        Log.d(TAG, "filePath: " + filePath);
+        recorder.setOutputFile(filePath);
+        recorder.setMaxDuration(50000); // 50 seconds
+        recorder.setMaxFileSize(5000000); // Approximately 5 megabytes
+    }
+
+    private void prepareRecorder() {
+        recorder.setPreviewDisplay(holder.getSurface());
+
+        try {
+            recorder.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            finish();
+        } catch (IOException e) {
+            e.printStackTrace();
+            finish();
+        }
+    }
+
+    public void onClick(View v) {
+        if (recording) {
+            recorder.stop();
+            recording = false;
+
+            // Let's initRecorder so we can record again
+//            initRecorder();
+//            prepareRecorder();
+        } else {
+            recording = true;
+            recorder.start();
+        }
+    }
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        prepareRecorder();
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+            int height) {
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (recording) {
+            recorder.stop();
+            recording = false;
+        }
+        recorder.release();
+        finish();
+    }
 }
+
